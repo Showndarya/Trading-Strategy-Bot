8ba468e1cb6a0d2966f17d936bfa73029dd642c5
Showndarya <shmadhav@bu.edu>
2023-11-01 07:57:00 -0400

    Save reddit data in csv

--- reddit_crawler.py
+++ reddit_crawler.py
@@ -1,4 +1,5 @@
 import praw
+import csv
 
 reddit = praw.Reddit(
     client_id='',      
@@ -7,7 +8,6 @@ reddit = praw.Reddit(
     username='',    
     password=''      
 )
-
 #sample query testing
 # Fetch the top 10 posts from the Python subreddit
 # top_posts = reddit.subreddit('Python').top(limit=10)
@@ -27,16 +27,27 @@ reddit = praw.Reddit(
 subreddits = ["investing", "stocks", "wallstreetbets", "Forex", "options"]
 keywords = ["strategy", "logic", "prediction", "probability"]
 
-for subreddit_name in subreddits:
-    print(f"Searching in r/{subreddit_name}:")
-    
-    subreddit = reddit.subreddit(subreddit_name)
-    
-    for keyword in keywords:
-        print(f"\nPosts with the keyword '{keyword}':")
-        search_results = subreddit.search(keyword, limit=10)
-        
-        for post in search_results:
-            print(f"{post.title} (Score: {post.score})")
-        
-        print("-" * 40)
+with open('reddit_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['keyword', 'subreddit', 'title', 'content', 'score']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    for subreddit_name in subreddits:
+        print(f"Searching in r/{subreddit_name}:")
+
+        subreddit = reddit.subreddit(subreddit_name)
+
+        for keyword in keywords:
+            print(f"\nPosts with the keyword '{keyword}':")
+            search_results = subreddit.search(keyword, limit=1000)
+
+            for post in search_results:
+                #print(f"{post.title} (Score: {post.score})")
+                writer.writerow({
+                    'keyword': keyword,
+                    'subreddit': subreddit_name,
+                    'title': post.title,
+                    'content': post.selftext,
+                    'score': post.score
+                })
