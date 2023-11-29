3212cdab094f15f52705c2cc8581d4d394219fe4
Showndarya <shmadhav@bu.edu>
2023-11-29 10:17:28 -0500

    Add code to create jsonl, model fine-tuning

--- model.py
+++ model.py
@@ -1,10 +1,42 @@
 import pandas as pd
+import json
+import openai
+openai.api_key = ''
 
-reddit_data = pd.read_csv('reddit_data.csv')
+df = pd.read_csv('data/gpt_training_data.csv')
 
-messages = reddit_data['title'].tolist()
-responses = reddit_data['content'].tolist()
+df['prompt'] = df['prompt'].astype(str).fillna('')
 
-# pip install transformers
+with open('data/fine_tune_data.jsonl', 'w') as jsonl_file:
+    for index, row in df.iterrows():
+        if isinstance(row['keywords'], list):
+            keywords = ', '.join(row['keywords'])
+        else:
+            keywords = row['keywords']
+        
+        data = {
+            "prompt": row['prompt'],
+            "completion": f"{row['answer']} [Sentiment: {row['sentiment_score']}] [Keywords: {keywords}]"
+        }
+        jsonl_file.write(json.dumps(data) + '\n')
 
-# can use this to fine tune gpt 3 https://github.com/norahsakal/fine-tune-gpt3-model/blob/main/fine_tune_step_by_step.ipynb
+
+
+result = openai.FineTune.create(
+    training_file='',   
+    model='gpt-3.5-turbo',            
+    n_epochs=3,                       
+    learning_rate_multiplier=0.1,     
+    batch_size=4,                     
+    prompt_loss_weight=0.1,           
+    compute_classification_metrics=False,  
+    # classification_n_classes=None,    
+    # classification_positive_class=None,  
+    classification_betas=(0.5, 0.5),  
+    # p=0.1,                            
+    # max_tokens=100,                   
+    use_packing=True,                 
+    # validation_file=None             
+)
+
+print(result)
